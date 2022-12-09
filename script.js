d325c6c360dfdd1bbfcfc334079f6e4101b24061
derekleehuang <derekh2008@gmail.com>
2022-12-09 11:12:57 -0700

    Add book modal, remove book, toggle read

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let myLibrary = [];
+let libraryIndex = 0;
 const booksGrid = document.querySelector('.cards-container');
 const addBookButton = document.querySelector('.add-book');
+const addBookModal = document.querySelector('.addBookModal');
+const addBookForm = document.querySelector('.add-book-form');
+const submitBookButton = document.querySelector('.addBookModal button');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -13,15 +17,15 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  let title = prompt("Title:");
-  let author = prompt("Author:");
-  let pages = prompt("Pages:");
-  let read = prompt("Read?");
+  // let title = prompt("Title:");
+  // let author = prompt("Author:");
+  // let pages = prompt("Pages:");
+  // let read = prompt("Read?");
   // read = read === 'true' || 'yes' ? true : false;
-  console.log(read);
-  if (read === 'false' || 'no') {
-    read = false;
-  }
+  // console.log(read);
+  // if (read === 'false' || 'no') {
+  //   read = false;
+  // }
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
   createBookCard(newBook);
@@ -32,19 +36,67 @@ function createBookCard(book) {
   const title = document.createElement('p');
   const author = document.createElement('p');
   const pages = document.createElement('p');
-  const read = document.createElement('p');
+  const read = document.createElement('button');
+  const remove = document.createElement('button');
 
   bookCard.classList.add('card')
   title.textContent = book.title;
   author.textContent = book.author;
   pages.textContent = `${book.pages} pages`;
   read.textContent = `${book.read ? "Read" : "Not read"}`;
+  remove.textContent = 'Remove';
+  remove.onclick = deleteBook;
+  read.onclick = toggleRead;
 
   bookCard.appendChild(title);
   bookCard.appendChild(author);
   bookCard.appendChild(pages);
   bookCard.appendChild(read);
+  bookCard.appendChild(remove);
+  bookCard.setAttribute('libraryIndex', libraryIndex);
+  libraryIndex++;
   booksGrid.appendChild(bookCard);
+}
+
+function toggleRead(e) {
+  let thisCard2 = e.target.parentNode;
+  let currentStatus = e.target.textContent;
+  e.target.textContent = `${currentStatus === 'Read' ? 'Not read' : "Read"}`;
+  console.log(thisCard2.getAttribute('libraryIndex'));
+  myLibrary[thisCard2.getAttribute('libraryIndex')].read = currentStatus === 'Read' ? false : true;
+}
+
+function deleteBook(e) {
+  let thisCard = e.target.parentNode;
+  myLibrary.splice(thisCard.dataset.libraryIndex, 1);
+  thisCard.remove();
+  console.log(e.target.parentNode);
+  libraryIndex--;
+  updateCardIndices();
+}
+
+function closeAddBookModal() {
+  addBookModal.classList.remove('active');
+}
+
+function addBook(e) {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('is-read').checked;
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  createBookCard(newBook);
+  closeAddBookModal();
+
+}
+
+function updateCardIndices() {
+  let childCards = booksGrid.children;
+  for (let i = 0; i < childCards.length; i++) {
+    childCards[i].setAttribute('libraryIndex', i);
+  }
 }
 
 function displayBooks() {
@@ -53,7 +105,10 @@ function displayBooks() {
   }
 }
 
-addBookButton.addEventListener("click", addBookToLibrary);
+addBookButton.addEventListener("click", () => {
+  addBookModal.classList.add('active');
+  addBookForm.reset();
+});
 
 const book1 = new Book('asdf', 'asdfw', 123, true);
 myLibrary.push(book1);
@@ -65,3 +120,5 @@ const book3 = new Book('asdf', 'asdfw', 123, true);
 myLibrary.push(book3);
 
 displayBooks();
+
+addBookForm.onsubmit = addBook;
